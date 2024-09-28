b057789f14283188f4aa854a00f7a7868c9a732c
Rômulo Bastos <romulobastos.design@gmail.com>
2024-09-28 10:38:24 -0300

    :tada: First release - Ionic 7.2 app test

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'primeiro-app',
+  appName: 'ionic-7.2-app-test',
   webDir: 'www'
 };
 
